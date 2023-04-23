671a6ea84d5bda0c855a6c3ce9bfe0d2b23fae8a
RITIK RANAUT <rranaut76@gmail.com>
2023-04-23 19:54:56 +0530

    Created Meals Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
+
+import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
