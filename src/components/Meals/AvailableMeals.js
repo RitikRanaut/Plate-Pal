63623de739f47ceecdc94f107f9a0312b40d632f
RITIK RANAUT <rranaut76@gmail.com>
2023-04-25 17:34:20 +0530

    Structure Available Meal Section

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -28,11 +30,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealItem = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealItem = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealItem}</ul>
+      <Card>
+        <ul>{mealItem}</ul>
+      </Card>
     </section>
   );
 };
