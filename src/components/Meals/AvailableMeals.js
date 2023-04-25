bf2639ae7d4fc77f9df018739cb13831147e8c03
RITIK RANAUT <rranaut76@gmail.com>
2023-04-25 18:25:47 +0530

    Update input form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,6 +33,7 @@ const AvailableMeals = () => {
   const mealItem = DUMMY_MEALS.map((meal) => (
     <MealItem
       id={meal.id}
+      key={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
