80b16af2462393d3407715e3f1850b469d0c2a2c
RITIK RANAUT <rranaut76@gmail.com>
2023-04-25 17:44:14 +0530

    Add button to add item

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import classes from "./MealItemForm.module.css";
+
+const MeatItemForm =(prosp)=>{
+    return (
+        <form className={classes.form}>
+            <input />
+            <button>+ Add</button>
+        </form>
+    );
+};
+
+export default MeatItemForm;
