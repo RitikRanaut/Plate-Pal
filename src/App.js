53ded8a9a0dd8dc1f3f7af7fde8fcc1becc68a28
RITIK RANAUT <rranaut76@gmail.com>
2023-08-11 18:31:32 +0530

    Add Cart Functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartShown(false);
+  };
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShownCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
         <header className={classes.header}>
             <h1>Plate Pal</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShownCart} />
         </header>
         <div className={classes["main-image"]}>
             <img src={mealsImage} alt="Delicious Food"/>
